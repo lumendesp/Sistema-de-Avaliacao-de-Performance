43423bae9d351d965c68a7c1b22e6bbeb10fb6ad
Marcos <marcosgabrielma5@gmail.com>
2025-06-18 11:22:15 -0300

    feat: add search logic and updated search bar

--- frontend/src/pages/manager/Status.tsx
+++ frontend/src/pages/manager/Status.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import searchIcon from "../../assets/search.png";
 import CollaboratorCard from "../../components/CollaboratorCard.tsx";
 import type { Collaborator } from "../../types/collaboratorStatus.tsx";
+import { useState } from "react";
 
 const collaborators: Collaborator[] = [
   {
@@ -47,24 +48,49 @@ const collaborators: Collaborator[] = [
 ];
 
 export default function Collaborators() {
+  const [search, setSearch] = useState("");
+
+  const filtered = collaborators.filter((collab) =>
+    collab.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
-      <div className="flex items-center gap-2 mb-4">
+    <div className="flex flex-col gap-4 w-full relative">
+      {/* Barra de busca */}
+      <div className="flex items-center gap-2 rounded-xl py-4 px-7 w-full bg-white/50">
         <input
           type="text"
           placeholder="Buscar por colaboradores"
-          className="flex-grow px-4 py-2 rounded-md bg-white shadow-sm border border-gray-300 focus:outline-none"
+          className="flex-1 outline-none text-sm font-normal text-[#1D1D1D]/75 placeholder:text-[#1D1D1D]/50 bg-transparent"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
         <button className="bg-teal-700 text-white p-2 rounded-md">
           <img src={searchIcon} alt="Buscar" className="w-5 h-5" />
         </button>
       </div>
 
-      <div className="space-y-3">
-        {collaborators.map((collaborator) => (
-          <CollaboratorCard key={collaborator.id} collaborator={collaborator} />
-        ))}
-      </div>
+      {/* Lista dos colaboradores */}
+      {search.trim() !== "" && (
+        <div className="absolute top-full mt-2 w-full bg-white rounded-xl shadow-md px-2 py-3 flex flex-col gap-2 z-10">
+          <p className="ml-2 text-sm font-semibold text-[#334155]">
+            Resultados
+          </p>
+
+          {filtered.length > 0 ? (
+            filtered.map((collaborator) => (
+              <CollaboratorCard
+                key={collaborator.id}
+                collaborator={collaborator}
+              />
+            ))
+          ) : (
+            <p className="text-sm text-[#1D1D1D]/50 p-2">
+              Nenhum colaborador encontrado.
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
