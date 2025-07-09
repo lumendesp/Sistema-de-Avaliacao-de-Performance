3bc4d5a6c87ca49572361c658ca18d11f90bf8d8
liviabion <llb@cin.ufpe.br>
2025-07-09 12:23:40 -0300

    feat: collaborator sidebar resposivity

--- frontend/src/layouts/CollaboratorLayout.tsx
+++ frontend/src/layouts/CollaboratorLayout.tsx
@@ -3,9 +3,9 @@ import SidebarCollaborator from '../components/Sidebar/SidebarCollaborator';
 
 const CollaboratorLayout = () => {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex">
       <SidebarCollaborator />
-      <main className="ml-64">
+      <main className="flex-1 md:ml-64 transition-all duration-300">
         <Outlet />
       </main>
     </div>
