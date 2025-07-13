321ce472a99ced0d4f1515d42727f6d2073b577c
Luísa <lumendes2001@gmail.com>
2025-07-13 20:21:47 -0300

    fix: translate login and unauthorized pages to portuguese and make them responsive

--- frontend/src/pages/login/Unauthorized.tsx
+++ frontend/src/pages/login/Unauthorized.tsx
@@ -13,27 +13,27 @@ const Unauthorized = () => {
   };
 
   return (
-    <div className="bg-[#08605e63] flex justify-center items-center h-screen w-full">
-      <div className="w-1/2 flex flex-col items-center gap-4">
-        <p className="font-medium text-6xl text-[#044e4c]">Access denied!</p>
-        <p className="mt-6 text-center text-2xl text-[#ffffff]">
-          We're sorry, but you do not have required permissions to access this
-          page. Please contact the site administrator.
+    <div className="bg-[#08605e63] flex justify-center items-center min-h-screen px-4 py-8 flex-col md:flex-row w-full">
+      <div className="w-full md:w-1/2 flex flex-col items-center gap-4 order-2 md:order-1">
+        <p className="font-medium text-4xl md:text-6xl text-[#044e4c]">Acesso negado!</p>
+        <p className="mt-6 md:max-w-[800px] w-full text-center text-lg md:text-2xl text-[#ffffff]">
+          Desculpe, você não tem permissão para acessar esta página. Por favor,
+          entre em contato com o administrador do sistema.
         </p>
         <div className="mt-2 flex gap-2 items-center">
-          <p className="text-[#044e4c]">Click to</p>
+          <p className="text-[#044e4c]">Clique para</p>
           <button
             onClick={handleLogout}
             className="bg-[#044e4c] hover:bg-[#144544] text-white px-6 py-2 rounded-xl"
           >
-            Logout
+            Sair
           </button>
         </div>
       </div>
       <img
         src={unauthorized}
         alt="Unauthorized Illustration"
-        className="w-1/2 max-w-xl"
+        className="order-1 md:order-2 w-1/2 max-w-xl"
       />
     </div>
   );
