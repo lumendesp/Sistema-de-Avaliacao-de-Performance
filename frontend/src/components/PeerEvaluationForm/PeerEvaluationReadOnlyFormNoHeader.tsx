9b5e867dd33269c2b701868ca99443d92d5bbb77
Marcos <marcosgabrielma5@gmail.com>
2025-07-13 09:28:17 -0300

    feat: update peer evaluation form in mentor

--- frontend/src/components/PeerEvaluationForm/PeerEvaluationReadOnlyFormNoHeader.tsx
+++ frontend/src/components/PeerEvaluationForm/PeerEvaluationReadOnlyFormNoHeader.tsx
@@ -22,21 +22,20 @@ const PeerEvaluationReadOnlyFormNoHeader = ({
   projects = [],
 }: PeerEvaluationReadOnlyFormNoHeaderProps) => {
   return (
-    <div className="bg-white w-full flex flex-col px-6 py-9 rounded-xl">
+    <div className="bg-white w-full flex flex-col px-6 py-9 rounded-xl opacity-80">
       <div className="flex flex-col mb-4 gap-3">
         <p className="font-medium text-xs text-opacity-75 text-[#1D1D1D]">
           Avaliação de um colega
         </p>
         <StarRatingReadOnly score={score ?? 0} dimmed={true} />
       </div>
-
-      <div className="flex gap-2 mb-4">
+      <div className="flex flex-col sm:flex-row gap-2 mb-4">
         <div className="flex flex-col gap-1 flex-1">
           <p className="font-medium text-xs text-opacity-75 text-[#1D1D1D]">
             Pontos fortes
           </p>
           <textarea
-            className="w-full h-24 resize-none p-2 rounded border border-gray-300 text-sm bg-gray-100"
+            className="w-full h-24 resize-none p-2 rounded border border-gray-300 text-sm bg-gray-100 text-[#1D1D1D]"
             value={strengths}
             readOnly
           />
@@ -46,17 +45,27 @@ const PeerEvaluationReadOnlyFormNoHeader = ({
             Pontos de melhoria
           </p>
           <textarea
-            className="w-full h-24 resize-none p-2 rounded border border-gray-300 text-sm bg-gray-100"
+            className="w-full h-24 resize-none p-2 rounded border border-gray-300 text-sm bg-gray-100 text-[#1D1D1D]"
             value={improvements}
             readOnly
           />
         </div>
       </div>
-
-      <div className="flex flex-wrap gap-2">
-        <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-2 mb-4">
+        <p className="font-medium text-xs text-opacity-75 text-[#1D1D1D]">
+          Você ficaria motivado em trabalhar novamente?
+        </p>
+        <input
+          type="text"
+          className="w-full h-9 p-2 rounded border border-gray-300 text-sm text-[#1D1D1D] bg-gray-100"
+          value={motivationLabel}
+          readOnly
+        />
+      </div>
+      <div className="flex flex-col sm:flex-row gap-2">
+        <div className="flex flex-col gap-1 flex-1">
           <p className="font-medium text-xs text-opacity-75 text-[#1D1D1D]">
-            Projeto em que atuaram juntos (obrigatório terem atuado juntos)
+            Projeto em que atuaram juntos
           </p>
           <input
             type="text"
@@ -65,26 +74,13 @@ const PeerEvaluationReadOnlyFormNoHeader = ({
             readOnly
           />
         </div>
-
-        <div className="flex flex-col gap-1">
-          <p className="font-medium text-xs text-opacity-75 text-[#1D1D1D]">
-            Você ficaria motivado em trabalhar novamente com este colaborador?
-          </p>
-          <input
-            type="text"
-            className="w-full h-9 p-2 rounded border border-gray-300 text-sm text-[#1D1D1D] bg-gray-100"
-            value={motivationLabel}
-            readOnly
-          />
-        </div>
-
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 flex-1">
           <p className="font-medium text-xs text-opacity-75 text-[#1D1D1D]">
             Período (em meses)
           </p>
           <input
             type="text"
-            className="w-full max-w-28 h-9 p-2 rounded border border-gray-300 text-sm text-[#1D1D1D] bg-gray-100"
+            className="w-full h-9 p-2 rounded border border-gray-300 text-sm text-[#1D1D1D] bg-gray-100"
             value={projects[0]?.period?.toString() || ""}
             readOnly
           />
